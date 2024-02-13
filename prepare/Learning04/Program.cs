f127c9da699441937087ffe0e56ea616f8351b5e
lcauffman <cau21005@byui.edu>
2024-02-12 18:14:25 -0700

    Update

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,26 @@
 using System;
 
-class Program
+
+namespace Learning04
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning04 World!");
+        static void Main(string[] args)
+        {
+            Assignment a1 = new Assignment("Logan Cauffman", "Multiplication");
+            Console.WriteLine(a1.GetSummary());
+
+            MathAssignment a2 = new MathAssignment("Logan Cauffman","Fractions", "7.3", "8-19");
+            Console.WriteLine(a2.GetSummary());
+            Console.WriteLine(a2.GetHomeworkList());
+
+            WritingAssignment a3 = new WritingAssignment("Logan Cauffman", "European History", "The Causes of WW2");
+            Console.WriteLine(a3.GetSummary());
+            Console.WriteLine(a3.GetWritingInformation());
+
+        }
     }
-}
+}    
+
+    
+
--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -1,66 +1,21 @@
+namespace Develop04;
+
 public class Activity
 {
-    protected string _name;
-    protected string _description;
-    protected int _duration;
-    public Activity(string name, string description)
-    {
-        _name = name;
-        _description = description;
-    }
+   public Activity(string name)
+   {
+        protected string _name;
+        protected string _description;
+        protected int _duration;
 
-    public void DisplayStartingMessage()
-    {
-        Console.Clear();
-        Console.WriteLine($"Prepare for {_name} Activity.\n");
-        Console.WriteLine(_description);
-        Console.WriteLine("Enter duration of time(SECONDS) for the activity:");
-        string input = Console.ReadLine();
-        _duration = int.Parse(input);
-    }
-
-    public void DisplayEndingMessage()
-    {
-        Console.WriteLine("\nCongratulations\n");
-        Console.WriteLine($"\n{_duration} seconds of {_name} complete.");
-    }
-
-    public void ShowSpinner(int seconds)
-    {
-        for (int i = 0; i < seconds; i++)
+        public Activity(string name, string description)
         {
-            Console.Write("|");
-            Thread.Sleep(250);
-            
-            Console.Write("\b \b");
-
-            
-            Console.Write("/");
-            Thread.Sleep(250);
-            
-            Console.Write("\b \b"); 
-
-            
-            Console.Write("-");
-            Thread.Sleep(250);
-            
-            Console.Write("\b \b"); 
-
-            
-            Console.Write("\\");
-            Thread.Sleep(250);
-            
-            Console.Write("\b \b");
+            _name = "";
+            _description = "";
         }
-    }
+        
+        
+      
+   }
 
-    public void ShowCountDown(int seconds)
-    {
-        for (int i = 0; i < seconds; i++)
-        {
-            Console.Write(seconds - i);
-            Thread.Sleep(1000);
-            Console.Write("\b \b");
-        }
-    }
-}
+   
